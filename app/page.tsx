466a0d24b50e3c071f616f6ccb8bff4014d29ca1
Pertwerth <98328150+Pertwerth@users.noreply.github.com>
2025-09-24 01:52:48 +0800

    Added metadata to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,13 @@ import { PhotoGrid } from "@/components/photo-grid"
 import { Footer } from "@/components/footer"
 import { FloatingActionButton } from "@/components/floating-action-button"
 import TrainerTabsSection from '@/components/trainer-tabs-section'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+      title: "All Dogs Training Co. - Houston Texas Homepage",
+      description: "This is the homepage for All Dogs Training Co., Houston TX, where you'll find everything you need about dog training straight from professional dog trainers.",
+      keywords: "dog training houston, dog trainer houston, dog training houston texas, best dog trainer houston, best dog training houston, best dog training houston tx, train dog houston, dog training service houston, puppy training houston, obedience training houston",
+}
 
 
 export default function HomePage() {
