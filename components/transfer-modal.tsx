9a60b942a60bb4dcc7dd3f1cede16f2291eef101
Pertwerth <98328150+Pertwerth@users.noreply.github.com>
2025-09-25 22:21:08 +0800

    Updated modal content

--- components/transfer-modal.tsx
+++ components/transfer-modal.tsx
@@ -6,19 +6,39 @@ export default function TransferModal() {
     return(
         <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-100">
       {/* Modal box */}
-      <div className="bg-white rounded-2xl shadow-xl p-6 w-full max-w-md text-center">
+      <div className="bg-white rounded-2xl shadow-xl p-6 w-full max-w-md">
         <div className="flex w-full justify-center items-center py-2">
             <Image
                 src='/images/logo.png'
                 alt="All Dogs Training Co Logo"
-                height={300}
-                width={300}
+                height={150}
+                width={150}
             />
         </div>
-        <h2 className="text-xl font-bold mb-4">Welcome to All Dogs Training Co.</h2>
-        <p className="mb-4 whitespace-normal pb-12">This is our old site. We now have new sites for our Austin, Houston and our upcoming Georgia locations</p>
+        <h2 className="text-xl font-bold mb-4 self-start pl-8 text-accent">The Good Dog Garage</h2>
+        <p className="mb-4 whitespace-normal pb-2 text-center">Your one stop shop for 
+            <span className="text-accent font-bold"> ALL THINGS DOG</span>!!!
+        </p>
 
-        <p className="mb-4 whitespace-normal">Please click on a button below to go to the site for our location closest to you!</p>
+        <p className="mb-4 whitespace-normal pb-2">Home of ALL Dogs Training Co. in Houston, Austin and coming soon... <span className="text-accent font-bold">Villa Rica, GA!</span></p>
+        <p className="mb-4 whitespace-normal pb-2">Serving families in the Atlanta Area and Peachtree City, Fayetteville and all surrounding areas!</p>
+        <p className="mb-4 whitespace-pre-line pd-2">
+            Custom Training Programs:
+            <br />
+            {'\t'} • Board and Train Programs 
+            <br />
+            {'\t'} • Behavior Solutions 
+            <br />
+            {'\t'} • Day School 
+            <br />
+            {'\t'} • Boarding 
+            <br />
+            And <span className="text-accent font-bold">COMING SOON</span> To all locations...
+            <br />
+            <p className="ml-5 text-center">{'\t\t'} THE <span className="text-accent font-bold">CLEAN SLATE PROGRAM</span> by Fabian Robinson and Gina Bishop.</p>
+        </p>
+
+        <p className="mb-4 whitespace-normal pb-2">Here are the links to our Houston and Austin locations. Georgia site to come soon!</p>
 
         <div className="flex justify-center gap-2">
             <Link href="https://www.alldogstraininghouston.com/">
