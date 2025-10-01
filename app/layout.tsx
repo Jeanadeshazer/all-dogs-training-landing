f265ab4710f87ffa591f3c8878fbc94a84fe6b57
Pertwerth <98328150+Pertwerth@users.noreply.github.com>
2025-10-02 00:48:05 +0800

    Added google tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,27 +44,47 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Load the gtag.js library */}
+        {/* Google Tag Manager */}
         <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTAG_ID}`}
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer', '${process.env.NEXT_PUBLIC_GTM_ID}');
+            `,
+          }}
+        />
+         <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GADS_ID}`}
           strategy="afterInteractive"
         />
-
-        {/* Initialize gtag */}
         <Script
-          id="ga-init"
+          id="gtag-init"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_GTAG_ID}');
+              gtag('config', '${process.env.NEXT_PUBLIC_GADS_ID}');
             `,
           }}
         />
       </head>
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <Suspense fallback={null}>{children}</Suspense>
         <Analytics />
         {/* Load GA script */}
